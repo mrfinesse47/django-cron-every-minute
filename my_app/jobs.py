8c1475298eb9ec1b07004de26322c4c863ee96dd
Kevin Mason <krmason2020@gmail.com>
2023-01-01 16:52:56 -0800

    first

---
+++ my_app/jobs.py
@@ -0,0 +1,6 @@
+from django.utils import timezone
+
+
+
+def some_task():
+    print("hello cron5, ",timezone.now())
---
+++ my_app/scheduler.py
@@ -0,0 +1,17 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from .jobs import some_task
+def start_jobs():
+
+    print("starting scheduled cron jobs")
+    
+    scheduler = BackgroundScheduler()
+    
+    #Set cron to runs every 20 min.
+    cron_job = {'month': '*', 'day': '*', 'hour': '*', 'minute':'*/1'}
+    
+    #Add our task to scheduler.
+    scheduler.add_job(some_task, 'cron', **cron_job)
+    
+
+#And finally start.    
+    scheduler.start()
